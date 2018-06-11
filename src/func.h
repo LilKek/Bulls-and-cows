aea06d5db105728e7541d6ebc621e2500130fc52
admin <admin@localhost.localdomain>
2018-06-11 19:39:35 +0700

    gameplay's functions head file

---
+++ src/func.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int *str_to_int(char *s, int n);
+
+int input_correct(int *A);
+
+int *gen_number ();
+
+int count_bulls (int *A, int *B);
+
+int count_cows (int *A, int *B);
